9fa31af0ec9deeeff1a1ad4f83d91dba39afcdbd
Hoseong-Ryu <52982845+Hoseong-Ryu@users.noreply.github.com>
2020-09-21 09:46:27 +0900

    onclick리스너(미완성)

--- app/src/main/java/com/example/gsmgosu/MainActivity.kt
+++ app/src/main/java/com/example/gsmgosu/MainActivity.kt
@@ -1,32 +1,42 @@
 package com.example.gsmgosu
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    var modelList = ArrayList<FieldModel>()
+data class InitData(val imageResource : Int, val name: String)
 
-    private lateinit var recyclerAdapter: FieldAdapter
+
+class MainActivity : AppCompatActivity() {
+    var initDataList = arrayListOf<InitData>(
+        InitData(R.drawable.icon_mobile_app, "모바일 앱"),
+        InitData(R.drawable.icon_design, "디자이너"),
+        InitData(R.drawable.icon_web, "웹"),
+        InitData(R.drawable.icon_server, "서버"),
+        InitData(R.drawable.icon_cloud, "클라우드"),
+        InitData(R.drawable.icon_game_develope, "게임 개발"),
+        InitData(R.drawable.icon_hardware, "하드웨어"),
+        InitData(R.drawable.icon_etc, "기타")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        this.modelList.add(FieldModel(name = "IOS", profileImage = R.drawable.chooseios))
+        init()
+    }
 
-        recyclerAdapter = FieldAdapter()
+    private fun init() {
+        val linearLayoutManager = LinearLayoutManager(this)
+        val gridLayoutManager = GridLayoutManager(this, 4)
 
-        recyclerAdapter.submitList(this.modelList)
+        main_recyclerView.layoutManager = gridLayoutManager
+        val adapter = RecyclerViewAdapter(applicationContext, initDataList)
 
-        // 리사이클러뷰 설정
-        dataList.apply {
+        main_recyclerView.adapter = adapter
 
-            // 리사이클러뷰 방향 등 설정
-            layoutManager = GridLayoutManager(this@MainActivity,2, GridLayoutManager.VERTICAL, false)
-
-            // 어답터 장착
-            adapter = recyclerAdapter
-        }
     }
 }

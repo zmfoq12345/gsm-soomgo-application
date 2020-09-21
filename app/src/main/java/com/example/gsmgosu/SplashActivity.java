bb8c74bab725fc0d414ddebb087536280e64fb38
zmfoq12345 <anihyun1026@naver.com>
2020-09-22 08:58:05 +0900

    Update SplashActivity.java

--- app/src/main/java/com/example/gsmgosu/SplashActivity.java
+++ app/src/main/java/com/example/gsmgosu/SplashActivity.java
@@ -31,7 +31,7 @@ public class SplashActivity extends Activity {
 //            }else{
                   // 회원가입으로 이동
 //            }
-            startActivity(new Intent(getApplication(), MainActivity.class));
+            startActivity(new Intent(getApplication(), ChatActivity.class));
             SplashActivity.this.finish();
         }
     }
